ad3039bea7480056dcb833992a00bf387db3fdf0
Donizete Crisostomo <donizete112@gmail.com>
2022-10-07 23:52:46 -0300

    solucao

--- Python-Desafios-Iniciais Py-Unimed-BH/Desafios-Iniciais Py-Unimed-BH/2_cachorros_quentes.py
+++ Python-Desafios-Iniciais Py-Unimed-BH/Desafios-Iniciais Py-Unimed-BH/2_cachorros_quentes.py
@@ -15,11 +15,14 @@ Você pode ajudá-los? Eles prometeram pagá-lo com um dos seus saborosos cachor
 determinar o número médio de cachorros-quentes consumidos pelos participantes.
 
 Entrada
-A entrada consiste de uma única linha que contém dois inteiros H e P (1 ≤ H, P ≤ 1000) indicando respectivamente 
+A entrada consiste de uma única linha que contém dois inteiros H e P (1 ≤ H, P ≤ 1000) indicando 
+respectivamente 
 o número total de cachorros-quentes consumidos e o número total de participantes na competição.
 
 Saída
-Seu programa deve produzir uma única linha com um número racional representando o número médio de cachorros-quentes consumidos pelos participantes. O resultado deve ser escrito como um número racional com exatamente dois dígitos após o ponto decimal, arredondado se necessário.
+Seu programa deve produzir uma única linha com um número racional representando o número médio de 
+cachorros-quentes consumidos pelos participantes. O resultado deve ser escrito como um número racional 
+com exatamente dois dígitos após o ponto decimal, arredondado se necessário.
 
 ============================================
 | Exemplo de Entrada | Exemplo de Saída    |
