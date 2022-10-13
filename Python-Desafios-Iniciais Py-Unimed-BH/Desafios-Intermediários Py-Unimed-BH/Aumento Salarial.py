34797613e179f23ee3ab35928c4d990c23b83365
Donizete Crisostomo <donizete112@gmail.com>
2022-10-13 19:29:01 -0300

    delete repositorio

--- Python-Desafios-Iniciais Py-Unimed-BH/Desafios-Intermediários Py-Unimed-BH/Aumento Salarial.py
+++ Python-Desafios-Iniciais Py-Unimed-BH/Desafios-Intermediários Py-Unimed-BH/Aumento Salarial.py
@@ -1,4 +1,4 @@
-A empresa que você trabalha resolveu conceder um aumento salarial a todos funcionários, de acordo com a tabela abaixo:
+""" A empresa que você trabalha resolveu conceder um aumento salarial a todos funcionários, de acordo com a tabela abaixo:
 
 ============================================
 | Salário |      Percentual de Reajuste     |
@@ -25,7 +25,8 @@ Exemplo 1
 |       1000     |    Novo salario: 1120,00 | 
 |                |    Reajuste ganho: 120,00|
 |                |    Em percentual: 12 %   |
-|-------------------------------------------|
+|                                           |
+--------------------------------------------|
 
 
 Exemplo 2
@@ -36,4 +37,42 @@ Exemplo 2
 |       1000     |    Novo salario: 2200,00 | 
 |                |    Reajuste ganho: 200,00|
 |                |    Em percentual: 10 %   |
+|                                           |
 |-------------------------------------------|
+ """
+
+import os
+os.system("cls")  # limpa tela
+
+salario = int(input())
+
+
+if(salario <= 600):
+    reajuste = salario * 0.17
+    novo_valor = + salario
+    percentual = 17
+
+
+elif(salario) >= 600.01 and (salario) <= 900:
+    reajuste = salario * 0.13
+    novo_valor = + salario
+    percentual = 13
+
+
+elif(salario) >= 900.01 and (salario) <= 1500:
+    reajuste = salario * 0.12
+    novo_valor = salario + reajuste
+    percentual = 12
+
+
+elif(salario) >= 1500.01 and (salario) <= 2000:
+    reajuste = salario * 0.10
+    novo_valor = salario + reajuste
+    percentual = 10
+
+
+else:
+    novo_valor = salario + (salario * (5/100))
+    reajuste = novo_valor - salario
+    percentual = 5
+print(f'Novo salario: {"{:.2f}".format(novo_valor)}\nReajuste ganho: {"{:.2f}".format(reajuste)}\nEm percentual: {percentual} %')
